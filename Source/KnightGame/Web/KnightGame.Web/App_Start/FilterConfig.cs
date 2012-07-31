71b314b063f578e6b27db3d67ecad549ef0fbc24
Akihiro Takada <umebayashi.akihiro@gmail.com>
2012-07-31 10:49:23 +0900

    KnightGame

---
+++ Source/KnightGame/Web/KnightGame.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace KnightGame.Web
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
